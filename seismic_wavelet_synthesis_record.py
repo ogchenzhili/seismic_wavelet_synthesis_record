afc4ea2424d5859d81be47f08784521c6ede8550
czl <1173446138@qq.com>
2019-05-01 10:17:04 +0800

    添加衰减系数

--- seismic_wavelet_synthesis_record.py
+++ seismic_wavelet_synthesis_record.py
@@ -19,13 +19,13 @@ class synthetic_seismic_record():
         def acquire_data(self):        
                 files = xlrd.open_workbook(self.filename)
                 sheet1 = files.sheet_by_name("Sheet1")
-                self.row=sheet1.nrows-1
-                self.col=sheet1.ncols 
-                for i in range(1,self.row+1):            
+                self.row = sheet1.nrows-1
+                self.col = sheet1.ncols 
+                for i in range(1, self.row+1):            
                         self.depth.append(sheet1.col_values(0)[i])
                         self.density.append(sheet1.col_values(1)[i])
                         self.velocity.append(sheet1.col_values(2)[i])
-                for i in range(0,self.row-1):
+                for i in range(0, self.row-1):
                         pv2 = self.density[i+1]*self.velocity[i+1]
                         pv1 = self.density[i]*self.velocity[i]
                         self.reflection_coefficient_sequence.append((pv2-pv1)/(pv1+pv2))
@@ -48,16 +48,19 @@ class synthetic_seismic_record():
                                 t_list.append(2*((self.depth[i]-self.depth[i-1])/self.velocity[i])+t_list[-1])
                 reflection_c_s=np.zeros(round(t_list[-1]/dt)+1)
                 for i in range(len(self.reflection_coefficient_sequence)):
+                        attenuation_coefficient = 1
+                        for j in range(i):
+                                attenuation_coefficient = attenuation_coefficient*(1 - self.reflection_coefficient_sequence[j]**2)
                         reflection_c_s[round(t_list[i]/dt)] = self.reflection_coefficient_sequence[i]
 
                 #卷积
-                result = np.convolve(reflection_c_s,ricker)
+                result = np.convolve(reflection_c_s, ricker)
                 #作图
                 T = np.array(range(len(result)))
                 power = np.array(result)
-                xnew = np.linspace(T.min(),T.max(),300) 
-                power_smooth = spline(T,power,xnew)
-                plt.plot(xnew,power_smooth)
+                xnew = np.linspace(T.min(), T.max(),300) 
+                power_smooth = spline(T, power, xnew)
+                plt.plot(xnew, power_smooth)
                 plt.show()
 
                 
@@ -73,7 +76,7 @@ class synthetic_seismic_record():
                                 d1.append(-self.depth[i])                       
                         v1.append(self.velocity[i])
                         v1.append(self.velocity[i])
-                plt.plot(v1,d1)
+                plt.plot(v1, d1)
                 plt.title("depth-velocity")
                 plt.show()
 
@@ -83,7 +86,7 @@ class synthetic_seismic_record():
                 d1 = []
                 v1 = []
                 for i in range(self.row-1):
-                        if i==0:
+                        if i == 0:
                                 d1.append(0)
                                 d1.append(-self.depth[i])
                         else:
@@ -97,7 +100,7 @@ class synthetic_seismic_record():
                 ricker = []
                 for t in range(100):
                         ricker.append(math.exp(-(math.pi*fm*(t*dt-1/fm))**2)*(1-2*(math.pi*fm*(t*dt-1/fm))**2))
-                #反射系数
+                # 时间序列
                 t_list = []
                 for i in range(self.row-1):
                         if i == 0:
@@ -105,31 +108,33 @@ class synthetic_seismic_record():
                                 t_list.append(2*(self.depth[i]/self.velocity[i]))
                         else:
                                 t_list.append(2*((self.depth[i]-self.depth[i-1])/self.velocity[i])+t_list[-1])
+                # 反射系数序列
                 reflection_c_s=np.zeros(round(t_list[-1]/dt)+1)
                 for i in range(len(self.reflection_coefficient_sequence)):
-                        reflection_c_s[round(t_list[i]/dt)] = self.reflection_coefficient_sequence[i]
+                        attenuation_coefficient = 1
+                        for j in range(i):
+                                attenuation_coefficient = attenuation_coefficient*(1 - self.reflection_coefficient_sequence[j]**2)
+                        reflection_c_s[round(t_list[i]/dt)] = self.reflection_coefficient_sequence[i]*attenuation_coefficient
 
                 #卷积
-                result = np.convolve(reflection_c_s,ricker)
+                result = np.convolve(reflection_c_s, ricker)
                 #作图
                 T = np.array(range(len(result)))
                 power = np.array(result)
-                xnew = np.linspace(T.min(),T.max(),300) 
-                power_smooth = spline(T,power,xnew)
+                xnew = np.linspace(T.min(), T.max(), 300) 
+                power_smooth = spline(T, power, xnew)
 
                 plt.subplot(223)
-                plt.plot(v1,d1,'r')
+                plt.plot(v1, d1, 'r')
                 plt.subplot(221)
                 plt.plot(ricker)
                 plt.subplot(222)
                 plt.stem(ricker)
                 plt.subplot(224)
-                plt.plot(xnew,power_smooth,'b')
+                plt.plot(xnew, power_smooth, 'b')
                 plt.show()
 
 
 if __name__ == "__main__":
-        a=synthetic_seismic_record("/home/xxx/data.xlsx")
-        # a.depth_velocity_graph()
-        # a.ricker_synthesis_graph()
+        a=synthetic_seismic_record("/home/czl/code/python/data.xlsx")
         a.all_graph()
